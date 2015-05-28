1f065078fcec2532e0dd241e8c4ad7234606adb1
Pablo <pablo@zonafrancavigo.com>
2015-05-28 15:18:18 +0200

    Añadimos el controlador quiz y las plantillas de pregunta y respuesta asociadas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var quizes = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
+
+router.get('/quizes/question',quizes.question);
+router.get('/quizes/answer',quizes.answer);
 
 module.exports = router;
