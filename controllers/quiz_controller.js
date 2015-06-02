98d1dd68c7e342399000b01e7452aa0069f823ee
pablo <pablo@Enterprise.StarFleet.org>
2015-06-02 21:16:52 +0200

    Implementamos la lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,16 +1,44 @@
-var models = require('../models/models.js');
+var models = require( '../models/models.js' );
 
-exports.question = function (req, res) {
-    models.Quiz.findAll().then(function (quiz) {
-        res.render('quizes/question', {pregunta: quiz[0].pregunta});
-    });
+// GET /quizes
+exports.index = function ( req , res )
+{
+  models.Quiz.findAll().then(
+    function ( quizes )
+    {
+      res.render( 'quizes/index' , { quizes : quizes } );
+    }
+  );
+}
+
+// GET /quizes/:quizId
+exports.show = function ( req , res )
+{
+  models.Quiz.findById( req.params.quizId ).then(
+    function ( quiz )
+    {
+      res.render( 'quizes/show' , { quiz : quiz } );
+    }
+  );
 };
 
-exports.answer = function (req, res) {
-    models.Quiz.findAll().then(function (quiz) {
-        res.render(
-            'quizes/answer',
-            {respuesta: req.query.respuesta === quiz[0].respuesta ? 'Correcto' : 'Incorrecto'}
-        );
-    });
+// GET /quizes/:quizId/answer
+exports.answer = function ( req , res )
+{
+  models.Quiz.findById( req.params.quizId ).then(
+    function ( quiz )
+    {
+      res.render(
+        'quizes/answer' ,
+        {
+          quiz : quiz ,
+          respuesta : (
+            (
+              req.query.respuesta === quiz.respuesta
+            ) ? 'Correcto' : 'Incorrecto'
+          )
+        }
+      );
+    }
+  );
 }
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,25 @@
-var express = require('express');
+var express = require( 'express' );
 var router = express.Router();
 
-var quizes = require('../controllers/quiz_controller');
+var quizes = require( '../controllers/quiz_controller' );
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz' });
-});
+router.get(
+  '/' , function ( req , res , next )
+  {
+    res.render( 'index' , { title : 'Quiz' } );
+  }
+);
 
-router.get('/quizes/question',quizes.question);
-router.get('/quizes/answer',quizes.answer);
+router.get( '/quizes' , quizes.index );
+router.get( '/quizes/:quizId(\\d+)' , quizes.show );
+router.get( '/quizes/:quizId(\\d+)/answer' , quizes.answer );
 
-router.get('/author',function(req,res,next){
-  res.render('author')
-});
+router.get(
+  '/author' , function ( req , res , next )
+  {
+    res.render( 'author' )
+  }
+);
 
 module.exports = router;
