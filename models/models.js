a2564a81a624c90091353774fbb0ee99cda5fde0
pablo <pablo@Enterprise.StarFleet.org>
2015-06-02 20:40:39 +0200

    Modificado el modelo para soportar SQLite y Postgresql simultáneamente

--- models/models.js
+++ models/models.js
@@ -1,10 +1,34 @@
 var path = require('path');
 
+// Postgresql DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite     DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var dbName = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
 // Cargar modelo ORM
 var Sequelize = require('sequelize');
 
 // Usar DB SQLite
-var sequelize = new Sequelize(null, null, null, {dialect: "sqlite", storage: "quiz.sqlite"});
+var sequelize = new Sequelize(
+    dbName,
+    user,
+    pwd,
+    {
+        dialect: dialect,
+        protocol: protocol,
+        port: port,
+        host: host,
+        storage: storage, // solo SQLite (.env)
+        omitNull: true    // solo Postgresql
+    }
+);
 
 // Importar la definición de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
